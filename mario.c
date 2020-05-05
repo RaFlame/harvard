43d1efa67af4734adc5a315e5e738eaa3964cc33
raflame <raflame@users.noreply.github.com>
2020-05-05 08:11:50 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,29 +1,34 @@
-// Prints 3-by-3 grid of bricks
+// Prints a right angled pyramid
 #include <cs50.h>
 #include <stdio.h>
 
+// Here i choose the data type 
 int main(void)
 
 {
-    int n,i,k,j;
+//adding names to data type to create the variables
+    int n, i, k, j;
     do
     {
+// Creating a do loop to request user for integer
         n = get_int("Size: ");
     }
+//Making sure user can only input values between the positive integers 1 and 8
     while (n < 1 || n > 8);
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for(k = 7; k >= j; k--)
-        
-            printf(" \n");
-        
-        for(j = 0; j <= i; j++)
+// this for loop makes sure the pyramid will be angled to the right       
+        for (k = 7; k >= j; k--)
         {
-            printf("#\n");
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++)
+        {
+            printf("#");
         
         }
         
-        
+        printf("\n");
     }
 }
     
