f426a1256c21646c266d52335a66a3d2dd75fa17
raflame <raflame@users.noreply.github.com>
2020-05-16 19:47:54 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -3,9 +3,11 @@ from cs50 import get_int
 #Get user input
 while True:
     n = get_int("Height: ")
-    if n >= 1 and n <= 8:
+    if n >= 0 and n <= 23:
         break
         
 for i in range(n):
     print(' ' * (n-1-i), end="")
+    print('#' * (i + 1), end="")
+    print('  ', end= "")
     print('#' * (i + 1))
