eb1a5db46284881c531092998a5ec6dde6098f07
raflame <raflame@users.noreply.github.com>
2020-05-20 08:31:42 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -2,30 +2,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_positive_int(void);
 // Here i choose the data type 
 int main(void)
-
 {
 //adding names to data type to create the variables
-    int n, i, k, j;
+    int n;
     do
     {
 // Creating a do loop to request user for integer
-        n = get_int("Size: ");
+        n = get_int("Height: ");
     }
 //Making sure user can only input values between the positive integers 1 and 8
     while (n < 1 || n > 8);
-    for (i = 0; i < n; i++)
+    
+    for (int i = 0; i < n; i++)
     {
 // this for loop makes sure the pyramid will be angled to the right       
-        for (k = 7; k >= j; k--)
+        for (int j = 0; j < n; j++)
         {
-            printf(" ");
-        }
-        for (j = 0; j <= i; j++)
-        {
-            printf("#");
+            if (i + j < n - 1)
+                printf(" ");
+            else
+                printf("#");
         }
         printf("\n");
     }
