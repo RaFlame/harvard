8b602a328fbbbe7c1d43e8771b9ac8fb6ffb6b8c
raflame <raflame@users.noreply.github.com>
2020-05-20 07:24:13 +0000

    automated commit by submit50

--- 9.sql
+++ 9.sql
@@ -1,7 +1,6 @@
-SELECT DISTINCT(people.name)
-FROM stars
-JOIN people ON stars.person_id = people.id 
-JOIN movies ON stars.movie_id = movies.id 
+SELECT DISTINCT(people.name) FROM stars
+JOIN people ON stars.person_id = people.id
+JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year = 2004
-ORDER BY birth DESC;
+ORDER BY birth;
 
