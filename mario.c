f93736377b32f4b7bd5c291e587e769302f370a0
raflame <raflame@users.noreply.github.com>
2020-05-20 08:33:48 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -21,9 +21,13 @@ int main(void)
         for (int j = 0; j < n; j++)
         {
             if (i + j < n - 1)
+            {
                 printf(" ");
+            }
             else
+            {
                 printf("#");
+            }
         }
         printf("\n");
     }
