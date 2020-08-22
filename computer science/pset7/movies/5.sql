5cfc8181737045c1a9b3054914abb25fecf59728
raflame <raflame@users.noreply.github.com>
2020-08-22 09:14:47 +0000

    automated commit by submit50

--- computer science/pset7/movies/5.sql
+++
@@ -1,4 +0,0 @@
-SELECT "year", "title" 
-FROM "movies"
-WHERE "title" LIKE 'Harry Potter%'
-ORDER BY "year" ASC
--- computer science/pset7/movies/9.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT(people.name) FROM stars
-JOIN people ON stars.person_id = people.id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
-ORDER BY birth;
-
